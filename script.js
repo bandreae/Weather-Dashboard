507d8a205df256ef08b05a8a26a84f6822e7c681
andrbj <brandonjandreae@gmail.com>
2021-04-14 18:59:01 -0500

    weather degree

--- script.js
+++ script.js
@@ -25,17 +25,22 @@
 
 console.log("hello world!");
 
-var CityApi = document.querySelector("#search");
-var Btnsearch = document.querySelector("#btnsearch");
+var cityApi = document.querySelector("#search");
+var btnSearch = document.querySelector("#btnsearch");
+var currentDiv = document.querySelector("#current-weather-forecast")
+var forecastDiv = document.querySelector("#forecast-container")
 var pastCity = "";
 var iconEl; 
 
 
 function currentWeatherApi(event) {
   event.preventDefault();
+  //console.log("currentWeatherApi function called")
+  var cityValue = cityApi.value;
+  //console.log(cityValue)
   fetch(
     "https://api.openweathermap.org/data/2.5/weather?q=" +
-      CityApi.value +
+      cityValue +
       "&units=imperial&appid=855d76da096afb4f6b03794b5572ca06"
   )
     .then(function (response) {
@@ -46,6 +51,7 @@ function currentWeatherApi(event) {
       var humidity = document.querySelector(".humidity");
       var wind = document.querySelector(".wind");
       var cityName = document.querySelector(".city");
+      var weatherIconEl = document.querySelector(".weathericon");
       var dateForToday = new Date(data.dt * 1000).toLocaleDateString("en-US");
       var lat = data.coord.lat;
       var lon = data.coord.lon;
@@ -53,43 +59,76 @@ function currentWeatherApi(event) {
 
       console.log(data);
       console.log(data.main.temp);
-      weatherIconEl.src =
-        "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
-      weatherIconEl.style = "width: 45px; height: 45px; display: inline-block;";
-      cityName.textContent = data.name + " " + dateForToday;
 
-      temperature.textContent = "Temperature: " + data.main.temperature + "°F";
-      humidity.textContent = "Humidity: " + data.main.humidity + "%";
-      wind.textContent = "Wind Speed: " + data.wind.speed + " MPH";
+currentDiv.innerHTML =
+  `
+  <h3 id="city">
+  <span id="current-city-forecast">
+    </span><span id="current-date"></span><span id="weather-icon"></span>
+</h3>
 
-      localStorage.setItem("SearchedCity", CityApi.value);
+<div> Temperature: ${data.main.temp}</div>
+<div id="humidity"></div>
+<div id="wind"></div>
+<div id="UVindex"></div>
+`
+
+      
+
+      //weatherIconEl.src =
+        //"https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
+     // weatherIconEl.style = "width: 45px; height: 45px; display: inline-block;";
+      //cityName.textContent = data.name + " " + dateForToday;
+
+     // temperature.textContent = "Temperature: " + data.main.temperature + "°F";
+     // humidity.textContent = "Humidity: " + data.main.humidity + "%";
+      //wind.textContent = "Wind Speed: " + data.wind.speed + " MPH";
+
+      localStorage.setItem("SearchedCity", cityApi.value);
+    fiveDayForecast(data.coord.lat, data.coord.lon) 
+
     });
 }
 
 function fiveDayForecast(lat, lon) {
-  fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,current,alerts&units=imperial&appid=73764a83501606e04b74a8e0281a654b")
+  fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,current,alerts&units=imperial&appid=855d76da096afb4f6b03794b5572ca06")
       .then(function (response) {
           console.log(response);
           return response.json();
       })
       .then(function (data) {
-          for (i = 1; i < 6; i++) {
+        console.log(data)
+          for (i = 0; i < data.daily.length; i++) {
+            console.log(data.daily[i])
               var imgIcon = document.createElement("img")
               var Icon = document.getElementById(i);
 
               var newDate = new Date(data.daily[i].dt*1000).toLocaleDateString("en-US");
               console.log(newDate);
-              Icon.textContent = newDate;
+              //Icon.textContent = newDate;
 
               imgIcon.setAttribute("src", "https://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + "@2x.png");
               imgIcon.style = "width: 45px; height: 45px; display: inline-block;"
-              Icon.append(imgIcon);
+              //Icon.append(imgIcon);
 
               var weatherDiv = document.createElement("div");
               weatherDiv.innerHTML = "Temp: " + data.daily[i].temp.day + "°F" + " " + "Humidity: " + data.daily[i].humidity + "%";
-              Icon.append(weatherDiv);
+
+              forecastDiv.innerHTML = 
+              `
+              <div class="col-6 col-md-2 forecast">${new Date(data.daily[0].dt*1000).toLocaleDateString("en-US")}</div>
+              
+              <div class="col-6 col-md-2 forecast">${new Date(data.daily[1].dt*1000).toLocaleDateString("en-US")}</div>
+              <div class="col-6 col-md-2 forecast">${new Date(data.daily[2].dt*1000).toLocaleDateString("en-US")}</div>
+              <div class="col-6 col-md-2 forecast">${new Date(data.daily[3].dt*1000).toLocaleDateString("en-US")}</div>
+              <div class="col-6 col-md-2 forecast">${new Date(data.daily[4].dt*1000).toLocaleDateString("en-US")}</div>
+              `
+
+              //Icon.append(weatherDiv);
           }
-          document.querySelector(".forecast").hidden = false;
-          document.querySelector(".card-group").hidden = false;
+          //document.querySelector(".forecast").hidden = false;
+          //document.querySelector(".card-group").hidden = false;
       });
 }
+
+btnSearch.addEventListener("click", currentWeatherApi )
